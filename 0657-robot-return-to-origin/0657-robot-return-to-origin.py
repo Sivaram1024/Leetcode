5295f405fa914f6deb3153fd20233f73da1e6855
Sivaram <s3378254547@gmail.com>
2026-06-05 19:08:13 +0530

    Time: 0 ms (100%), Space: 19.2 MB (51.41%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        if moves.count('U') == moves.count('D') and moves.count('R') == moves.count('L'):
+            return True
+        return False
+            
